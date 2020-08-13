b39ff25e1ca1d092908b2b58a6195b39ad9bf474
Volha Dounar <volha.dounar@yandex.by>
2020-08-13 22:36:28 +0300

    my second commit

--- posts/admin.py
+++ posts/admin.py
@@ -14,6 +14,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ("text",) 
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",) 
+    empty_value_display = "-пусто-"
 
 # при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
